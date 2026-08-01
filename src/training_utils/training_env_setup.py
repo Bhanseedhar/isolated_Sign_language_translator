819149701610448d6447df3b920ea593c5ffaae6
Bhansee <janakibaskarsaibhanseedhar@gmail.com>
2026-08-01 13:08:51 +0530

    random bug

--- src/training_utils/training_env_setup.py
+++ src/training_utils/training_env_setup.py
@@ -1,20 +1,21 @@
 import gc
-from src.config import seed_everything
+from src.config import seed_everything, CFG
 from tensorflow.keras import mixed_precision
+
 
 
 def setup_training_env(CFG):
     """
     Prepare the TF training env
     """ 
-    seed_everything(cfg.seed)
+    seed_everything(CFG.seed)
     #clear previous TF session
     tf.keras.backend.clear_session()
 
     gc.collect()  # Run Python garbage collection
     tf.config.optimizer.set_jit(True)  # Enable XLA JIT compilation
 
-    if cfg.fp16:
+    if CFG.fp16:
         try:
             policy = mixed_precision.Policy("mixed_bfloat16")
         except:
